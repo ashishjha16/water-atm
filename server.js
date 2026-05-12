7f6b8c5156b2842bcd434ed60c92b00fdc2805c0
ashishjha16 <ashishjha7628@gmail.com>
2026-05-12 23:37:33 +0530

    change api key

--- server.js
+++ server.js
@@ -202,5 +202,20 @@ app.use((req, res) => {
   res.status(404).json({ error: "Not found." });
 });
 
-module.exports = app;
+app.post("/webhook/razorpay", express.json(), (req, res) => {
 
+  console.log("Webhook received");
+
+  console.log(req.body);
+
+  res.status(200).json({
+    success: true
+  });
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
